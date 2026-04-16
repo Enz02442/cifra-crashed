7aadcee1151e742e01733bfe1b838b2e1ea356c8
Enzo <enzobicalho94@gmail.com>
2026-04-16 09:27:56 -0300

    Trabalho

---
+++ Cifrador.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+ import java.text.Normalizer;
+
+public class Cifrador {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+String f = "Quero ser programador. Aprendi Java no Devisate. Marilia";
+System.out.println(f);
+
+
+    }
+}
